13dd61b0979fc1e5e32bfea46c818f8fbc4ea2b4
username <178353419+while-maybe@users.noreply.github.com>
2024-11-15 01:08:50 +0000

    feat: add models.py

---
+++ ecommerce/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Order(models.Model):
+    customer_name = models.CharField(max_length=100)
+    product_name = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    order_date = models.DateField(default=timezone.now)
+    
+    class Meta:
+        ordering = ['-order_date']
+    
+    def __str__(self):
+        return f"{self.customer_name} ordered {self.quantity}X {self.product_name} on {self.order_date}"
