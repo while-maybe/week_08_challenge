ce3dc2a59c25a9d470aa6e054b80e8e1bcb54fd7
username <178353419+while-maybe@users.noreply.github.com>
2024-11-19 16:52:20 +0000

    feat: add title case to relevant form entries

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -8,5 +8,12 @@ class OrderForm(forms.ModelForm):
         model = Order
         fields = ['customer_name', 'product_name', 'quantity', 'order_date']
 
-        
-    
+    def clean_customer_name(self):
+        customer_name = self.cleaned_data.get('customer_name')
+        customer_name = customer_name.strip().title()
+        return customer_name
+
+    def clean_product_name(self):
+        product_name = self.cleaned_data.get('product_name').title()
+        product_name = product_name.strip()
+        return product_name
