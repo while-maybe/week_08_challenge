32af81b88cdabc86d5572aaf5522471db1074733
username <178353419+while-maybe@users.noreply.github.com>
2024-11-19 04:40:05 +0000

    style: fix form max-width

--- ecomm_project/settings.py
+++ ecomm_project/settings.py
@@ -127,7 +127,7 @@ DATE_FORMAT = 'd/m/Y'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # added here
 STATIC_ROOT = BASE_DIR / 'static'
 
