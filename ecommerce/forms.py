00787679d180172db51b61b34db0de4833f3f3c9
username <178353419+while-maybe@users.noreply.github.com>
2024-11-19 01:03:33 +0000

    style: fix visible fields in form

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -7,15 +7,6 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['customer_name', 'product_name', 'quantity', 'order_date']
-        # widgets = {
-        #     'date_field': DateInput(
-        #         attrs={
-        #             'type': 'date',
-        #             'class': 'form-control',
-        #             # needed to send correct date format to html 5
-        #             'data-date-format': 'yyyy-mm-dd'
-        #         }
-        #     )
-        # }
+
         
     
